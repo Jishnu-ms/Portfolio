5caae7c6d4c09082fd58363c7c77cd5754ef218f
jishnu <jishnums830@gmail.com>
2024-11-03 13:22:02 +0530

    made improvements 1

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     alert("Message sent! Thank you for reaching out.");
     document.getElementById("contact-form").reset();
 });
+
+function toggleMenu() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+}
+
+// Close the menu when a link is clicked
+document.querySelectorAll(".nav-links .txt").forEach(link => {
+    link.addEventListener("click", () => {
+        document.getElementById("nav-links").classList.remove("show");
+    });
+});
+
